486b1827a7f6c8b0cc230f75ea943547d15d17f9
guptas627 <102958127+guptas627@users.noreply.github.com>
2025-03-02 17:47:48 +0000

    Add files via upload

---
+++ src/main/java/com/prolink/ProLinkApplication.java
@@ -0,0 +1,13 @@
+package com.prolink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProLinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prolink/ProLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prolink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
