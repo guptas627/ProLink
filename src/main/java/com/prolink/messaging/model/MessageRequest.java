798c0714311dd42c64fc7dcc98df9702ff3e472f
guptas627 <102958127+guptas627@users.noreply.github.com>
2025-03-28 21:38:51 +0000

    Update MessageRequest.java

--- src/main/java/com/prolink/messaging/model/MessageRequest.java
+++ src/main/java/com/prolink/messaging/model/MessageRequest.java
@@ -9,4 +9,9 @@ public class MessageRequest {
     private String sender;
     private String receiver;
     private String content;
+    public MessageRequest(String sender, String receiver, String content) {
+        this.sender = sender;
+        this.receiver = receiver;
+        this.content = content;
+    }
 }
